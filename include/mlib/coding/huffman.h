0f8f76af66be7b5bedc6142f1b9ce7095829977e
ESeNonFossiIo <esenonfossiio@gmail.com>
2018-08-02 22:04:19 +0100

    Init Huffman

---
+++ include/mlib/coding/huffman.h
@@ -0,0 +1,16 @@
+#ifndef _MLIB_HUFFMAN_
+#define _MLIB_HUFFMAN_
+
+/** \addtogroup coding
+ *  @{
+ */
+
+namespace mlib
+{
+
+};
+
+
+/** @}*/
+
+#endif //_MLIB_HUFFMAN_
---
+++ include/mlib/data_structures/node.h
@@ -0,0 +1,36 @@
+#ifndef _MLIB_DATA_STRUCTURES_
+#define _MLIB_DATA_STRUCTURES_
+
+#include <memory>
+
+/** \addtogroup data_structure
+ *  @{
+ */
+
+namespace mlib
+{
+
+template<typedef VALUE>
+class Node
+{
+public:
+    Node(){};
+
+    Node(std::shared_ptr<Node> right_, std::shared_ptr<Node> left_)
+    :
+        right(right_),
+        left(left_){};
+
+private:
+    VALUE val;
+
+    std::shared_ptr<Node> right;
+    std::shared_ptr<Node> left;
+}
+
+};
+
+
+/** @}*/
+
+#endif //_MLIB_DATA_STRUCTURES_
