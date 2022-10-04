dfc48b9e2fa1cbf977965896b83a716133fcf5ad
ESeNonFossiIo <esenonfossiio@gmail.com>
2022-10-04 18:47:27 +0100

    Clean README

--- python/mlibpy.cmake
+++ python/mlibpy.cmake
@@ -4,8 +4,8 @@ FILE(COPY "./python/mlibpy" DESTINATION "${CMAKE_BINARY_DIR}")
 
 # Activate conda environment, assume Anaconda or Miniconda is already installed
 IF(EXISTS ${CMAKE_BINARY_DIR}/mlibpy_env)
-    EXECUTE_PROCESS(COMMAND conda activate myenv)
+    EXECUTE_PROCESS(COMMAND conda activate ${CMAKE_BINARY_DIR}/mlibpy_env)
 ELSE()
     EXECUTE_PROCESS(COMMAND conda create --yes --quiet --prefix ${CMAKE_BINARY_DIR}/mlibpy_env  python=3.6)
-    EXECUTE_PROCESS(COMMAND conda activate mlibpy_env)
+    EXECUTE_PROCESS(COMMAND conda activate ${CMAKE_BINARY_DIR}/mlibpy_env)
 ENDIF()
