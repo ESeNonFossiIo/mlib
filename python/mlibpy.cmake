ca1a1775e1266c9c98c69953058f504eb102c071
ESeNonFossiIo <esenonfossiio@gmail.com>
2022-10-06 18:06:23 +0100

    Add support for python tests

--- python/mlibpy.cmake
+++ python/mlibpy.cmake
@@ -4,8 +4,9 @@ FILE(COPY "./python/mlibpy" DESTINATION "${CMAKE_BINARY_DIR}")
 
 # Activate conda environment, assume Anaconda or Miniconda is already installed
 IF(EXISTS ${CMAKE_BINARY_DIR}/mlibpy_env)
+    EXECUTE_PROCESS(COMMAND conda env update -f ${CMAKE_BINARY_DIR}/../python/mlibpy_env.yml --prefix ${CMAKE_BINARY_DIR}/mlibpy_env)
     EXECUTE_PROCESS(COMMAND conda activate ${CMAKE_BINARY_DIR}/mlibpy_env)
 ELSE()
-    EXECUTE_PROCESS(COMMAND conda create --yes --quiet --prefix ${CMAKE_BINARY_DIR}/mlibpy_env  python=3.6)
+    EXECUTE_PROCESS(COMMAND conda env create -f ${CMAKE_BINARY_DIR}/../python/mlibpy_env.yml --prefix ${CMAKE_BINARY_DIR}/mlibpy_env)
     EXECUTE_PROCESS(COMMAND conda activate ${CMAKE_BINARY_DIR}/mlibpy_env)
 ENDIF()
