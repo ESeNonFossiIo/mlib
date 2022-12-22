d543cb0dd54715e2855ef9a0cd93e687cae5b545
ESeNonFossiIo <esenonfossiio@gmail.com>
2022-12-22 14:22:18 +0000

    test

--- python/mlibpy.cmake
+++ python/mlibpy.cmake
@@ -3,8 +3,8 @@ FILE(COPY "./python/mlibpy" DESTINATION "${CMAKE_BINARY_DIR}")
 # find_package(Python REQUIRED)
 
 # Activate conda environment, assume Anaconda or Miniconda is already installed
-OPTION( BUILD_PY_ENV
-"Create a new python ENV" ON)
+OPTION(BUILD_PY_ENV
+"Create a new python ENV" OFF)
 IF(BUILD_PY_ENV)
   EXECUTE_PROCESS(COMMAND conda deactivate)
   IF(EXISTS ${CMAKE_BINARY_DIR}/mlibpy_env)
