4bef03f48191bbc73d19fcd77b53d08d1cda0ab1
Mauro Bardelloni <ESeNonFossiIo@users.noreply.github.com>
2025-01-02 18:16:07 +0000

    improve doc and indent

--- include/mlib/finance/black_scholes.h
+++ include/mlib/finance/black_scholes.h
@@ -8,26 +8,26 @@
 
 namespace mlib
 {
-
   namespace finance
   {
 
-    // Calculate the European vanilla call price based on
-    // underlying S, strike K, risk-free rate r, volatility of
-    // underlying sigma and time to maturity T
+    // @brief Calculate the price of a European call option
+    // @details This function calculates the price of a European call option based on the Black-Scholes model.
+    // The Black-Scholes model is a mathematical model used for pricing options contracts.
+    // It is based on the assumption that the price of the underlying asset follows a geometric Brownian motion
+    // with constant volatility and that the risk-free interest rate is known and constant.
+    // The model takes into account the current price of the underlying asset, the strike price of the option,
+    // the risk-free interest rate, the volatility of the underlying asset, and the time to maturity of the option.
     double BSPrice(
-      const double S, ///< [in] underlying
-      const double K, ///< [in] strike
-      const double r, ///< [in] risk-free rate
-      const double v, ///< [in] volatility of underlying sigma
-      const double T, ///< [in] time to maturity
-      const OptionType optType ///< [in] option to compute
+        const double S,          ///< [in] current price of the underlying asset
+        const double K,          ///< [in] strike price of the option
+        const double r,          ///< [in] risk-free interest rate
+        const double v,          ///< [in] volatility of the underlying asset
+        const double T,          ///< [in] time to maturity of the option
+        const OptionType optType ///< [in] type of option (European call or put)
     );
-
   }
 
-
 }
-
 
 /** @}*/
